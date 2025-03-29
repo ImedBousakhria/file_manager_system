4eb10bb2e7131f708c8aa6a4ca169f76dc7d91d2
imedBousakhria <i_bousakhria@estin.dz>
2025-03-29 22:10:24 +0100

    virtual disk creation

--- src/disk.c
+++ src/disk.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <disk.h>
+#include "fs.h"
 
-int main (int argc, char *argv[]) {
-    return EXIT_SUCCESS;
+FILE *disk_file;
+FileSystem fs_metadata;
+
+int disk_init() {
+    if(!fopen('fs_vdisk.img', 'rb+')) {
+        disk_file = fopen('fs_vdisk.img', 'wb+');
+        
+        if (!disk_file) return -1;
+        // treat
+    } else {
+       fread(&fs_metadata, sizeof(FileSystem) , 1, disk_file);
+    }
+    return 0;
 }
+
---
+++ src/disk.h
@@ -0,0 +1,17 @@
+#ifndef DISK_H 
+#define DISK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BLOCK_SIZE 512
+#define NUM_BLOCKS 1024
+#define DISK_NAME "fs_vdisk.img"
+
+int disk_init();
+int disk_read(int block_num, void *buf);
+int disk_write(int block_num, const void *buf); 
+void disk_format();  
+
+
+#endif
