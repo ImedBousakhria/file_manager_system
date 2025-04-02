3153ab187fc1b5b8a36baeafe836aa7c54a4296d
imedBousakhria <i_bousakhria@estin.dz>
2025-04-02 02:44:10 +0200

    write in blocks

--- src/file.c
+++ src/file.c
@@ -18,7 +18,7 @@ int find_free_inode() {
         }
     }
     // don't forget to increament the nb_inodes when you create the inode if i == nb_inodes + 1 
-    return nb_inodes + 1; 
+    return nb_inodes; 
 }
 
 // Function to find the directory index corresponding to an inode
@@ -43,8 +43,8 @@ int find_directory_index(const char *path) {
 
         // Unique loop, because we're jump searching (not sequencial)
         for (int i = 0; i < fs_metadata.directories[current_dir_index].num_entries; i++) {
-            if (strcmp(fs_metadata.directories[current_dir_index].entries[i].name, token) == 0) {
-
+            DirectoryEntry *entry = &fs_metadata.directories[current_dir_index].entries[i];
+            if (strcmp(entry->name, token) == 0  && !entry->isfile ) {
                 current_dir_index = fs_metadata.directories[current_dir_index].entries[i].inode_index;
                 found = 1;
                 break;
@@ -98,7 +98,7 @@ int entry_exists(char *name, int dir_index, int isfile){
 
 // here i have to change the second param to be a path then we look for its index with the function
 // for now the user is an int 
-void create_file(const char *filename, char* parent_path, int user_index) {
+int create_file(const char *filename, char* parent_path, int user_index) {
     
     // get out if parent_path does not exist 
     int dir_idx = find_directory_index(parent_path);
@@ -107,7 +107,7 @@ void create_file(const char *filename, char* parent_path, int user_index) {
         return;
     }
     
-    //check if there is a file with the same name in the dir 
+    // check if there is a file with the same name in the dir 
 
     if(entry_exists(filename, dir_idx, 1)){
         printf("Error: a file with the same name already exists");
@@ -153,4 +153,96 @@ void create_file(const char *filename, char* parent_path, int user_index) {
     entry->isfile = 1; // Mark as file
 
     printf("File '%s' created successfully in directory :  '%s' .\n", filename, parent_path);
+    return new_inode_idx;
+}
+
+
+void write_to_file(const char *parent_path, const char *filename, const char *data){
+    // find the directory index from provided path
+    int parent_inode_idx = find_directory_index(parent_path);
+    int file_inode_idx;
+    if (parent_inode_idx == -1){
+        printf("Path is not valid");
+        return ;
+    }
+
+    // check if file exists in the path, if not, create it
+    if(!entry_exists(filename, parent_inode_idx, 1)){
+        printf("File does not exist");
+        file_inode_idx = create_file(filename, parent_path, 1);
+    }
+    
+
+    // allocate blocks to write DATA from input
+    int size = strlen(data);
+    
+    Inode *file_inode = &fs_metadata.inodes[file_inode_idx];
+
+    int file_size = file_inode->size;
+    int last_block_index = file_size / BLOCK_SIZE;
+    int offset_in_last_block = file_size % BLOCK_SIZE;
+
+    int remaining_size = size;
+    int data_offset = 0;
+
+    FILE *disk_file = fopen("disk.img", "r+b");
+    if (!disk_file) {
+        printf("Error: Could not open disk file.\n");
+        return;
+    }
+
+    // Actually writing 
+    while (remaining_size > 0) {
+        int block_idx = -1;
+
+        // use last block if it's not 100% filled
+        if (last_block_index < 30 && file_inode->blocks[last_block_index] != 0) {
+            int used_space_in_last_block = file_inode->size % BLOCK_SIZE;
+    
+            if (used_space_in_last_block < BLOCK_SIZE) {
+                // There's still space in the last block, so continue writing there
+                block_idx = file_inode->blocks[last_block_index];
+            }
+        } 
+        // sinon, allocate a new block
+        else {
+            for (int j = 0; j < NUM_BLOCKS; j++) {
+                if (fs_metadata.free_blocks[j] == 0) {
+                    fs_metadata.free_blocks[j] = 1;
+                    file_inode->blocks[last_block_index] = j;
+                    block_idx = j;
+                    break;
+                }
+            }
+        }
+
+        if (block_idx == -1) {
+            printf("Error: No space left on disk.\n");
+            fclose(disk_file);
+            return;
+        }
+
+        // Seek to the correct position in the block
+        fseek(disk_file, block_idx * BLOCK_SIZE + offset_in_last_block, SEEK_SET);
+
+        // Write the necessary amount of data
+        int writable_size = (remaining_size < (BLOCK_SIZE - offset_in_last_block)) ? remaining_size : (BLOCK_SIZE - offset_in_last_block);
+        fwrite(data + data_offset, 1, writable_size, disk_file);
+
+        // Update counters
+        remaining_size -= writable_size;
+        data_offset += writable_size;
+        last_block_index++;
+        offset_in_last_block = 0; // Only the first block might have an offset
+    }
+
+    fclose(disk_file);
+
+    // Update file size
+    file_inode->size += size;
+
+    // Save changes
+    save_file_system();
+
+    printf("Successfully wrote %d bytes to file '%s'.\n", size, filename);
 }
