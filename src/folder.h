f909134a6b89d38b5389f65dc5e1a3519707edd1
imedBousakhria <i_bousakhria@estin.dz>
2025-04-02 23:40:59 +0200

    init .h

--- src/folder.h
+++ src/folder.h
@@ -1,1 +1,14 @@
-// Dyhia
+#ifndef DIR_H
+#define DIR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "fs.h"
+
+void create_directory(const char *dirname, const char* parent_path, int user_index, int permission);
+int chdir(const char *path);
+const char* getcwd();
+void delete_dir(int dir_index);
+
+#endif
