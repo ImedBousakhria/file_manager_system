9a41e9808774a00be5741665f014df0c7dde24fe
imedBousakhria <i_bousakhria@estin.dz>
2025-04-03 00:06:18 +0200

    main init

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "disk.h"
+#include "file.h"
+#include "fs.h"
+#include "permissions.h"
+#include "utils.h"
+
+int main (int argc, char *argv[]) {
+
+        
+    return EXIT_SUCCESS;
+}
